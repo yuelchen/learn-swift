c9a15e22073fd2ba8be28899987afc58f6a81d88
Yue L. Chen <12385495+yuelchen@users.noreply.github.com>
2020-02-03 19:47:09 -0500

    Update CheatSheetAnatomy.swift

--- CheatSheetAnatomy.swift
+++ CheatSheetAnatomy.swift
@@ -56,3 +56,13 @@ for variableName in low... high {
 
 for item in array {
 }
+
+// Randomization
+Int.random(in: 1...3) //1 is lower bound and 3 is upper bound where upper bound is inclusive (possible values: 1, 2, 3)
+Int.random(in: 1..<3) //1 is lower bound and 3 is upper bound where upper bound is exclusive (possible values: 1, 2)
+
+Float.random(in: 1...3) //1 is lower bound and 3 is upper bound where upper bound is inclusive (possible values: any number between 1 and 3, including 1 and 3)
+Bool.random() //possible values: only true or false
+
+// Randomize Array
+array.shuffle()
