fd8ab851a59ac87fb6e12b8f6da1e6715a3f833a
Yue L. Chen <12385495+yuelchen@users.noreply.github.com>
2020-02-03 19:41:52 -0500

    Create Anatomy.swift

---
+++ Anatomy.swift
@@ -0,0 +1,58 @@
+// This is a single line comment
+/* This is a multi-line comment */
+
+// Variable Instantiation and Initialization (var vs let)
+var greeting = "Hello World"  //reference variable called greeting which contains the string hello world (mutable)
+let zero = 0                  //constant reference variable called zero which contains the integer 0 (immutable)
+
+/* You can also assign type to your variables / constants
+var variableName : Type
+let constantName : Type
+*/
+
+
+/* Basic Data Types
+- String : "Hello World"
+- Int (Integer) : 27
+- Float (Floating Point Number) : 12.345
+- Double : 3.14159265359 
+- Bool (Boolean) : true / false
+- Array : [1, 2, 3]
+- Dictionary : [key : value]
+*/
+
+// Structures Anatomy
+struct MyStructure {
+}
+
+// Class Anatomy
+class myClass: SuperClass {
+}
+
+// Function Anatomy
+func myFunction(input: Type) -> Type {
+  //function content (do something)
+  return output  
+}
+
+// If Statement
+if condition {
+  //do something if condition is met
+} elseif otherCondition {
+  //do something if other condition is met
+} else {
+  //otherwise, do something else
+}
+
+// Switch Statement
+switch variableName {
+  case 1:
+    //do something if variableName matches 1
+}
+
+// Loops
+for variableName in low... high {
+}
+
+for item in array {
+}
